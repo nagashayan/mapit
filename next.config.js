af8d291820a685a266f2f9f6028efa39f6958c8f
Nagashayan <nagashayan1@gmail.com>
2025-01-14 19:16:57 -0500

    v0.1.4

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: '/mapit',  // Add the basePath here (replace 'yourrepository' with your actual GitHub repo name)
+  assetPrefix: '/mapit',
 };
 
 module.exports = nextConfig;
